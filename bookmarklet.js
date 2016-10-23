0b00dc5a0736d1228f5bfcffd9d800ff47dc67e5
mrpnkt <mr@mrpnkt.com>
2016-10-23 13:18:48 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function() {
-    var newcss = ('#blueBarDOMInspector, .UFIAddComment, .UFILikeLink, .UFIReplyLink, #pagelet_composer div._mp3, #page_recommendations_vertex_composer, #pagelet_composer div._mp, ._5lc6, button.selected, #u_0_1p > div._sa_._5vsi._ca7._192z > div > div, .comment_link, .share_action_link { display: none !important; } ul.uiList._4kg { height: 45px; } ');
+    var newcss = ('#blueBarDOMInspector, .UFIAddComment, .UFILikeLink, .UFIReplyLink, #pagelet_composer div._mp3, #page_recommendations_vertex_composer, #pagelet_composer div._mp, ._5lc6, button.selected, #u_0_1p > div._sa_._5vsi._ca7._192z > div > div, .comment_link, .share_action_link, .topbar, .follow-bar, .Icon--reply, .inline-reply-tweetbox, .dashboard-right { display: none !important; } ul.uiList._4kg { height: 45px; } .PermalinkOverlay-with-background { background: #f5f8fa; } ');
     if ("\v" == "v") {
         document.createStyleSheet().cssText = newcss
     } else {
